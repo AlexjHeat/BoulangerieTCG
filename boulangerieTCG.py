712ad2642cb816af318771785b2990ef1e17b0b9
test name <testemail>
2021-07-23 02:39:40 -0700

    Created cog directory
    -Created cog directory and all of the python files that will hold the bot commands.
    
    -Added boulangerieTCG.py functionality to load cogs

--- boulangerieTCG.py
+++ boulangerieTCG.py
@@ -1,12 +1,26 @@
+import os
 from discord.ext import commands
 from source import config
+from source import verify
 
 
-client = commands.Bot(command_prefix=config.COMMAND_PREFIX)
+bot = commands.Bot(command_prefix=config.COMMAND_PREFIX)
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('Bot is online.')
 
-client.run(config.DISCORD_TOKEN)
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./source/cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(config.DISCORD_TOKEN)
